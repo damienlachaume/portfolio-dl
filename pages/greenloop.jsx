97a2ba3111fc6af6e117d4b22c4c85a6d4c4f02a
Damien Lachaume <d.lachaume@protonmail.com>
2022-12-22 15:54:54 +0100

    update greenloop dapp url

--- pages/greenloop.jsx
+++ pages/greenloop.jsx
@@ -63,7 +63,7 @@ const greenloop = () => {
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://alyra-nft-marketplace.vercel.app"
+            href="https://greenloop-alyra.vercel.app/"
             target="_blank"
             rel="noreferrer"
           >
